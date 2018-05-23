3704d90d65642922d5fb767bb8e38633a72b07a9
Charlie Waite <cw5790@gmail.com>
2018-05-23 15:36:35 +0100

    Added list for our side navigation drawer

---
+++ src/assets/DrawerItems.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import HomeIcon from '@material-ui/icons/Home';
+import RadioIcon from '@material-ui/icons/Radio';
+import CollectionIcon from '@material-ui/icons/Collections';
+
+const drawerItems = [
+	{
+		icon: <HomeIcon />,
+		text: 'Home',
+	},
+	{
+		icon: <i className="fab fa-lastfm" />,
+		text: 'Scrobble manually',
+	},
+	{
+		icon: <CollectionIcon />,
+		text: 'Scrobble album',
+	},
+	{
+		icon: <RadioIcon />,
+		text: 'Scrobble from Radio',
+	},
+];
+
+export default drawerItems;
