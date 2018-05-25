8c8a7c0e42f97205bf464f6112221e6a4b39224b
Charlie Waite <cw5790@gmail.com>
2018-05-25 11:14:04 +0100

    Added home content cards to the homepage

---
+++ src/assets/homeContentCardData.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const homeContentItems = [
+	{
+		icon: <i className="fas fa-play" style={{ fontSize: '25px', color: '#c3000d' }} />,
+		content: 'Scrbbl allows you to scrobble individual songs, entire albums, songs you may have heard on the radio, as well as from a database. The only manual scrobbler that offers all these services.',
+	},
+	{
+		icon: <i className="far fa-calendar-alt" style={{ fontSize: '25px', color: '#c3000d' }} />,
+		content: 'Listened to an album a week ago to notice your scrobbler of choice didn\'t register it? No problem. Scrbbl allows you to register tracks with Last.FM up to a month in the past.',
+	},
+	{
+		icon: <i className="far fa-smile" style={{ fontSize: '25px', color: '#c3000d' }} />,
+		content: 'No paywalls. No limitations that require payment to remove. Scrbbl is completely free and allows you to scrobble as many tracks as you want*',
+	},
+];
+
+export default homeContentItems;
