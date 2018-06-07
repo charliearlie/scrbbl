fada7da5053203196aa84f14ca8ec94641978246
Charlie Waite <cw5790@gmail.com>
2018-06-07 22:27:27 +0100

    Added a TextInput component

---
+++ client/src/components/reusable/TextInput.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+	input: {
+		fontSize: '16px',
+		'&:after': {
+			borderBottomColor: '#c3000d',
+		},
+	},
+	textField: {
+		marginLeft: theme.spacing.unit,
+		marginRight: theme.spacing.unit,
+		width: 300,
+	},
+	textFieldContainer: {
+		padding: '4px',
+	},
+	label: {
+		fontSize: '18px',
+	},
+});
+
+const TextInput = (props) => {
+	const { classes } = props;
+
+	return (
+		<div className={classes.textFieldContainer}>
+			<TextField
+				{...props}
+				className={classes.textField}
+				InputProps={{
+					className: classes.input,
+				}}
+				InputLabelProps={{
+					className: classes.label,
+				}}
+			/>
+		</div>
+	);
+};
+
+export default withStyles(styles, { withTheme: true })(TextInput);
