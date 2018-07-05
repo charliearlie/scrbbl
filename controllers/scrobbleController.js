d87eadf1c2756d43b46f9886d18d280ff8a0c2c9
Charlie Waite <cw5790@gmail.com>
2018-07-05 22:31:00 +0100

    Back end changes for setting date and time on manual scrobble

--- controllers/scrobbleController.js
+++ controllers/scrobbleController.js
@@ -1,14 +1,34 @@
 const lastfm = require('../routes/lastfm');
+const moment = require('moment');
 
 exports.manualScrobble = (req, res) => {
     const track = req.body;
 	const status = { "success": false };
     let date = Math.floor((new Date()).getTime() / 1000) - 300;
     
-	if (track.datePlayed) {
-		let douche = track.datePlayed.slice(0, 10) + track.timePlayed.slice(10)
-		date = Number(moment(douche).format('X'));
-    }
+	if (track.date) {
+		const dateSplit = track.date.split('-');
+		let formattedDate;
+		if (track.time) {
+			const splitTime = track.time.split(":");
+			formattedDate = moment().set({ 
+				day: dateSplit[2] - 1, 
+				month: dateSplit[1] - 1, 
+				year: dateSplit[0],
+				hour: splitTime[0],
+				minute: splitTime[1],
+			}).format('X');
+		} else {
+			formattedDate = moment().set({ 
+				day: dateSplit[2] - 1, 
+				month: dateSplit[1] - 1, 
+				year: dateSplit[0],
+			}).format('X');
+		}
+		date = formattedDate;
+	}
+	
+	console.log('DATE', date);
     
 	lastfm.setSessionCredentials(track.userName, track.key); //Horrible hack until I sort sessions with this api
 	lastfm.track.scrobble({
