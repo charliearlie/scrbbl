c111e9da25f39b190b92171a5c79aca94c54953b
Charlie Waite <cw5790@gmail.com>
2018-07-26 08:43:13 +0100

    Add AlbumSearchResultInput component

---
+++ client/src/components/album/AlbumSearchResultInput.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+	track: {
+		padding: '16px',
+	},
+	trackInput: {
+		backgroundColor: theme.palette.common.white,
+		border: '1px solid #ced4da',
+		fontSize: 16,
+		padding: '10px 12px',
+		width: 'calc(100% - 24px)',
+		transition: theme.transitions.create(['border-color', 'box-shadow']),
+		'&:focus': {
+			borderColor: '#80bdff',
+			boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+		},
+	},
+});
+
+class AlbumSearchResultInput extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			trackName: props.trackName,
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.onBlur = this.onBlur.bind(this);
+	}
+
+	onBlur() {
+		const { trackNumber } = this.props;
+		this.props.handleTrackChange(this.state.trackName, trackNumber);
+	}
+
+	handleChange(event) {
+		const { value } = event.target;
+		this.setState({ trackName: value });
+	}
+
+	render() {
+		const { classes } = this.props;
+		return (
+			<input
+				type="text"
+				className={classes.trackInput}
+				onBlur={this.onBlur}
+				onChange={this.handleChange}
+				value={this.state.trackName}
+				{...this.props}
+			/>
+		);
+	}
+}
+
+AlbumSearchResultInput.propTypes = {
+	classes: PropTypes.objectOf().isRequired,
+	handleTrackChange: PropTypes.func.isRequired,
+	trackNumber: PropTypes.number,
+	trackName: PropTypes.string.isRequired,
+};
+
+AlbumSearchResultInput.defaultProps = {
+	trackNumber: 0,
+};
+
+export default withStyles(styles, { withTheme: true })(AlbumSearchResultInput);
