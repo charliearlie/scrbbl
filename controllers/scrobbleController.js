2695cdd361e050e75708d5769308f7fa65a74d6d
Charlie Waite <cw5790@gmail.com>
2018-09-04 09:55:52 +0100

    Improved manual scrobble on the server

--- controllers/scrobbleController.js
+++ controllers/scrobbleController.js
@@ -1,53 +1,27 @@
-const lastfm = require('../routes/lastfm');
 const _ = require('lodash');
 const moment = require('moment');
+const mongoose = require('mongoose');
+const Scrobble = mongoose.model('Scrobble');
+const lastfm = require('../routes/lastfm');
 
 exports.manualScrobble = (req, res) => {
     const track = req.body;
-	const status = { "success": false };
     let date = Math.floor((new Date()).getTime() / 1000) - 300;
-	
-	// I wrote this on a previous scrobbler when I first started programming. 
-	// Honestly don't even know what's happening
-	if (track.date) {
-		const dateSplit = track.date.split('-');
-		let formattedDate;
-		if (track.time) {
-			const splitTime = track.time.split(":");
-			formattedDate = moment().set({ 
-				day: dateSplit[2] - 1, 
-				month: dateSplit[1] - 1, 
-				year: dateSplit[0],
-				hour: splitTime[0],
-				minute: splitTime[1],
-			}).format('X');
-		} else {
-			formattedDate = moment().set({ 
-				day: dateSplit[2] - 1, 
-				month: dateSplit[1] - 1, 
-				year: dateSplit[0],
-			}).format('X');
-		}
-		date = formattedDate;
-	}
-    
-	lastfm.setSessionCredentials(track.userName, track.key); //Horrible hack until I sort sessions with this api
-	console.log(lastfm);
-	lastfm.track.scrobble({
-		'artist': track.artist,
-		'track': track.songTitle,
-		'timestamp': date,
-		'album': track.albumTitle
 
-	}, function (err, scrobbles) {
+	track.timestamp = date;
+    
+	lastfm.setSessionCredentials(track.user, track.key); //Horrible hack until I sort sessions with this api
+	lastfm.track.scrobble(track, function (err, scrobbles) {
 		if (err) {
-			return console.log('We\'re in trouble', err);
 			lastfm.setSessionCredentials(null, null);
-			return res.json(status.success);
+			return res.json(false);
 		}
-		status.success = true;
-		lastfm.setSessionCredentials(null, null);
-		return res.json(status.success);
+
+		const scrobble = new Scrobble(track);
+		scrobble
+			.save()
+			.then(() => res.json(true)) // This will return the number of times the user has scrobbled the track
+			.catch(err => { throw Error(err) });
 	});
 };
 
