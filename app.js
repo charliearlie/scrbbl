8e62b3bc6406061a58c77da1e342565d36620762
Charlie Waite <cw5790@gmail.com>
2018-09-07 15:42:32 +0100

    Enforce SSL on Heroku

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var enforce = require('express-sslify');
 
 var index = require('./routes/index');
 var api = require('./routes/api');
 var scrobble = require('./routes/scrobble');
 
 var app = express();
+app.use(enforce.HTTPS());
+app.use(enforce.HTTPS({ trustProtoHeader: true })); 
 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
