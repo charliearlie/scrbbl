43d41275bc2347bee257b860567031479e72c7cb
Charlie Waite <cw5790@gmail.com>
2018-09-08 15:06:27 +0100

    Heroku https (#12)
    Default Heroku to https

--- app.js
+++ app.js
@@ -8,16 +8,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const sslRedirect = require('heroku-ssl-redirect');
 
 var index = require('./routes/index');
 var api = require('./routes/api');
 var scrobble = require('./routes/scrobble');
 
 var app = express();
+app.use(sslRedirect());
 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
