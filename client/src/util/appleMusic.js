e868922f31b98dc2583f445c4e772422d1363dc7
Charlie Waite <charlie.waite@oddschecker.com>
2019-02-07 19:45:19 +0000

    Moved Apple Music request to a separate util  file

---
+++ client/src/util/appleMusic.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import Config from '../config/endpoints';
+
+export async function search(query, entity = song) {
+	const response = await axios.get(
+		`${Config.endpoints.albumSearch}?term=${query.replace(
+			' ',
+			'+',
+		)}&media=music&entity=${entity}`,
+	);
+	return entity === 'album' ? response.data.results : response.data.results[0];
+}
