1dafa5acf0600cd130c99a12e940cbe6b0cb97c3
Charlie Waite <charlie.waite@oddschecker.com>
2019-02-07 20:02:30 +0000

    Updated default entity param in apple music search

--- client/src/util/appleMusic.js
+++ client/src/util/appleMusic.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Config from '../config/endpoints';
 
-export async function search(query, entity = song) {
+export async function search(query, entity = 'song') {
 	const response = await axios.get(
 		`${Config.endpoints.albumSearch}?term=${query.replace(
 			' ',
@@ -10,3 +10,7 @@ export async function search(query, entity = song) {
 	);
 	return entity === 'album' ? response.data.results : response.data.results[0];
 }
+
+export default {
+	search,
+};
