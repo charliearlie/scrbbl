c3a1c3ef21c834c6e49562d39ec3f497803a0bb6
Charlie Waite <cw5790@gmail.com>
2019-03-21 09:16:00 +0000

    Add Radio Scrobble 'page'

---
+++ client/src/pages/RadioScrobble.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+const styles = theme => ({});
+
+function RadioScrobble(props) {
+	return <div>Radio</div>;
+}
+
+export default withStyles(styles, { withTheme: true })(RadioScrobble);
