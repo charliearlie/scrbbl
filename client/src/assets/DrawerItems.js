255cc4104ac47679b0997e6061a5c7c3480478cd
Charlie Waite <cw5790@gmail.com>
2019-04-08 19:35:39 +0100

    Add shortText to drawer items for bottom nav

--- client/src/assets/DrawerItems.js
+++ client/src/assets/DrawerItems.js
@@ -7,21 +7,25 @@ const drawerItems = [
 	{
 		icon: <HomeIcon />,
 		text: 'Home',
+		shortText: 'Home',
 		route: '/',
 	},
 	{
 		icon: <i className="fab fa-lastfm" style={{ fontSize: '25px' }} />,
 		text: 'Scrobble manually',
+		shortText: 'Manual',
 		route: '/manual',
 	},
 	{
 		icon: <CollectionIcon />,
 		text: 'Scrobble album',
+		shortText: 'Album',
 		route: '/album',
 	},
 	{
 		icon: <RadioIcon />,
 		text: 'Scrobble from Radio',
+		shortText: 'Radio',
 		route: '/radio',
 	},
 ];
