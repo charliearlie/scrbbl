f487022887c9e2a3b76741d8f086bd8e73136837
Charlie Waite <cw5790@gmail.com>
2019-07-17 14:22:51 +0100

    Fix import

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,7 @@ import useLocalStorage from './hooks/useLocalStorage';
 const ManualScrobble = lazy(() => import('./components/ManualScrobble'));
 const AlbumScrobble = lazy(() => import('./pages/AlbumScrobble'));
 const RadioScrobble = lazy(() => import('./pages/RadioScrobble'));
-const AlbumADay = lazy(() => import('./pages/album-a-day'));
+const AlbumADay = lazy(() => import('./pages/albumADay'));
 
 const drawerWidth = 280;
 
