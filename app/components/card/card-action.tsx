23caa0731380592429bccf3cd0a9d5339d02ef97
Charlie W <cw5790@gmail.com>
2023-05-05 14:33:32 +0100

    Add build and validate step to CI

--- app/components/card/card-action.tsx
+++ app/components/card/card-action.tsx
@@ -9,7 +9,7 @@ type Props = PropsWithChildren<CardContentProps>;
 export default function CardAction({ children, noPadding = false }: Props) {
   return (
     <div
-      className={`py-2 border-t border-gray-300 dark:border-gray-900 mt-auto ${
+      className={`mt-auto border-t border-gray-300 py-2 dark:border-gray-900 ${
         noPadding ? "" : "px-2"
       } hover:bg-gray-100 hover:dark:bg-gray-900`}
     >
--- app/components/card/card-header.tsx
+++ app/components/card/card-header.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 export default function CardHeader({ children }: PropsWithChildren<{}>) {
   return (
-    <h2 className="flex rounded-t-lg text-gray-200 bg-gray-900 p-2 text-center text-2xl font-bold">
+    <h2 className="flex rounded-t-lg bg-gray-900 p-2 text-center text-2xl font-bold text-gray-200">
       {children}
     </h2>
   );
