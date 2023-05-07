ea4d383de2d09e88102d0321fc5b969e11951961
Charlie W <cw5790@gmail.com>
2023-05-08 00:13:24 +0100

    Try to fix login on mobile

--- app/components/login-link-button.tsx
+++ app/components/login-link-button.tsx
@@ -1,8 +1,19 @@
-export default function LoginLinkButton() {
-  const isBrowser = typeof window !== "undefined";
+import { useEffect, useState } from "react";
 
-  const origin = isBrowser ? window.location.origin : "";
-  const redirectUrl = `${origin}/auth-redirect`;
+export default function LoginLinkButton() {
+  const [redirectUrl, setRedirectUrl] = useState("");
+
+  useEffect(() => {
+    const isBrowser = typeof window !== "undefined";
+    console.log(isBrowser);
+    if (isBrowser) {
+      const origin = window.location.origin;
+      const redirectUrl = `${origin}/auth-redirect`;
+
+      setRedirectUrl(redirectUrl);
+    }
+  }, []);
+
   return (
     <a
       href={`http://www.last.fm/api/auth/?api_key=5e51b3c171721101d22f4101dd227f66&cb=${redirectUrl}`}
