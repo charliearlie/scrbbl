47c84645668fb63c13824090f7bdcef203a6738a
Charlie W <cw5790@gmail.com>
2024-06-20 23:44:40 +0100

    Clean up log in page

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,6 +1,7 @@
-import type { LoaderArgs} from "@remix-run/node";
+import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { Card, CardContent } from "~/components/common/card";
 import LoginLinkButton from "~/components/common/login-link-button";
 
 export const loader = async ({ request }: LoaderArgs) => {
@@ -15,9 +16,13 @@ export default function Login() {
   const { redirectUrl } = useLoaderData<typeof loader>();
   console.log("frontendUrl", redirectUrl);
   return (
-    <div className="mt-10">
-      You have to log in to scrobble lad
-      <LoginLinkButton redirectTo={redirectUrl} />
-    </div>
+    <main className="container mt-12 bg-background p-2 md:max-w-4xl">
+      <Card>
+        <CardContent className="flex flex-col gap-4">
+          <h1>This feature requires you to be logged in with Last.FM</h1>
+          <LoginLinkButton redirectTo={redirectUrl} />
+        </CardContent>
+      </Card>
+    </main>
   );
 }
